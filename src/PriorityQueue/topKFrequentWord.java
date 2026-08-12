c6c6c7f917133e17fb352cc5d6a5a0325bf1a2d4
HemantDigarse <hemantdigarse882@gmail.com>
2026-08-12 21:31:23 +0530

    topKFrequentWords

--- src/PriorityQueue/topKFrequentWord.java
+++ src/PriorityQueue/topKFrequentWord.java
@@ -2,10 +2,10 @@ package PriorityQueue;
 import java.util.*;
 public class topKFrequentWord {
     public static void main(String[] args) {
-        String[] word={"i","love","leetcode","i","love","coding"};
+        String[] words={"i","love","leetcode","i","love","coding"};
         int k = 2;
         HashMap<String,Integer> map=new HashMap<>();
-        for(String str:word){
+        for(String str:words){
             map.put(str,map.getOrDefault(str,0)+1);
         }
         PriorityQueue<String> pq=new PriorityQueue<>((a,b)->{
